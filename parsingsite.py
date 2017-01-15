7d6b74ff32d93ca6144ca037d76c76fd284ad7b3
latish86 <artemzvaygzne@gmail.com>
2017-01-15 18:34:57 +0700

    Refused grab, rewrote the lxml and urllib.

--- parsingsite.py
+++ parsingsite.py
@@ -1,9 +1,6 @@
 # Прога ищет стримы русскоговорящих программистов на сайте
 # https://www.liveedu.tv/
 
-
-
-
 # elem.attrib - получаем атрибуты
 # elem.text - текстовые данные
 # elem.keys() - список атрибутов
@@ -11,70 +8,52 @@
 # etree.tounicode(elem) - сорцы в юникоде
 # etree.tostring(elem) - сорцы байтами
 
-def searchRusProgInLiveedu():
-    # Функция ищет стримы русскоговорящих программистов на сайте
-    # https://www.liveedu.tv/ и возвращает список названий и ссылок
+def search_rus_prog_in_liveedu_lxml_lib(country_filter=[]):
+    """ Функция ищет стримы русскоговорящих программистов на сайте
+    https://www.liveedu.tv/ и возвращает список названий и ссылок
+    Используется библиотека lxml """
 
-    # elem.attrib - получаем атрибуты
-    # elem.text - текстовые данные
-    # elem.keys() - список атрибутов
-    # elem.values() - список значений атрибутов
-    # etree.tounicode(elem) - сорцы в юникоде
-    # etree.tostring(elem) - сорцы байтами
+    import lxml.html
+    from urllib.request import (urlopen,  # Для получения страницы
+                                Request)  # Для создания запроса
+    # Контейнер для хранения стримов
+    streams = []
+    # Начальная страница сайта
+    main_url = 'https://www.liveedu.tv/livestreams'
 
-    from grab import Grab
-    from lxml import etree
+    # Создаем запрос
+    req = Request(main_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-    # list = []
-    # g = Grab()
-    # resp = g.go('https://www.liveedu.tv/livestreams/')
+    # Создаем объект парсинга
+    page = lxml.html.parse(urlopen(req))
 
-    # for elem in resp.tree.xpath('.//div[@class="browse-main-videos--item"]'):
-    #
-    #     # Преобразуем елемент в строку а потом в документ HTML
-    #     # для дальнейшего парсинга
-    #     elem = etree.HTML(etree.tounicode(elem))
-    #
-    #     flag = elem.xpath('//img[@class="country-flag"]')
-    #     title = elem.xpath('//span[@class="browse-main-videos--title"]/a/span')
-    #     link = elem.xpath('//a[@class="browse-main-videos--thumbnail  "]')
-    #
-    #     try:
-    #         flag_text = flag[0].attrib['alt']
-    #         if flag_text in ['Russia', 'Ukraine', 'Belarus']:
-    #             title_text = title[0].text
-    #             link_text = link[0].attrib['href']
-    #             list.append([title_text, link_text])
-    #
-    #     except:
-    #         pass
-    #
-    # return list
+    # получаем список элементов заданного класса
+    flag = page.getroot().find_class('browse-main-videos--item')
 
+    for elem in flag:
+        country = elem.cssselect('.country-flag')[0].attrib['alt']
+        if country_filter != []:
+            if country in country_filter:
+                title = elem.cssselect('.browse-main-videos--title a span')[0].text
+                link = elem.cssselect('.browse-main-videos--title a')[0].attrib['href']
+                streams.append({'country': country,
+                                'title': title,
+                                'link': link})
+        else:
+            title = elem.cssselect('.browse-main-videos--title a span')[0].text
+            link = elem.cssselect('.browse-main-videos--title a')[0].attrib['href']
+            streams.append({'country': country,
+                            'title': title,
+                            'link': link})
 
-    # С обьектом select работает метод html() - возвращает сорцы
-
-    g = Grab()
-    g.go('https://www.liveedu.tv/livestreams/')
-    for elem in g.doc.select('.//div[@class="browse-main-videos--item"]'):
-        for flag in elem.select('//img[@class="country-flag"]/@alt'):
-            #print(flag.text())
-            if flag.text() in ['Russia', 'Ukraine', 'Belarus']:
-                for title in elem.select('//span[@class="browse-main-videos--title"]/a/span'):
-                    print(title.text())
-                #print(flag.text())
-
-
-
-        # title = elem.xpath('//span[@class="browse-main-videos--title"]/a/span')
-        # link = elem.xpath('//a[@class="browse-main-videos--thumbnail  "]')
-
+    return streams
 
 if __name__ == '__main__':
+    country_filter = ['Russian',
+                      'Ukraine',
+                      'Belarus',
+                      'Germany']
 
-    result = open('result.txt', 'wt', encoding='utf-8')
-    for i in searchRusProgInLiveedu():
-        result.write('Title: ' + i[0] + '\n' + 'Link: https://www.liveedu.tv' + i[1] + '\n\n')
-    result.close()
-    print('Парсинг завершен.')
-    input('Нажмите кнопку чтобы закрыть...')
+    for i in search_rus_prog_in_liveedu_lxml_lib(country_filter):
+        print('Country: {}\nTitle; {}\n'.format(i['country'], i['title']))
+
